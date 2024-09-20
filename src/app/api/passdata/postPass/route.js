12c09c256ef953e52c13e592eebd8b745a26b46e
Numan Ahmad <139751760+Noman0114@users.noreply.github.com>
2024-09-20 05:15:28 -0700

    Update route.js

--- src/app/api/passdata/postPass/route.js
+++ src/app/api/passdata/postPass/route.js
@@ -1,32 +1,68 @@
 import { connect } from '@/dbConfig/dbConfig';
 import passData from '@/models/passModel';
 import { NextResponse } from 'next/server';
-
+import jwt from 'jsonwebtoken'; 
 connect();
 
 export async function POST(request) {
     try {
+
+
+//new update for getting loginuser password only.
+//i can store a user id when i add a new password.
+//and get the password while matching these id.
+
+
+// const token = request.cookies.get('token')?.value;
+
+// if (!token) {
+//     return NextResponse.json({
+//         error: 'Authentication token is missing.'
+//     }, { status: 401 });
+// }
+
+// // Decode the token to get the user ID
+// const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+// const userId = decodedToken.id;
+
+
+
+
+        const token = request.cookies.get('token')?.value;
+
+        if (!token) {
+            return NextResponse.json({
+                error: 'Authentication token is missing.'
+            }, { status: 401 });
+        }
+
+        // Decode the token to get the user ID
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+       
+        
+        const userId = decodedToken.id;
+
+
         const body = await request.json();
         const { email, password, info } = body;
 
-        console.log(body);
-        console.log('In a blog route');
+      
         
         // Create and save the new blog post
         const newPass = new passData({
            email,
            password,
            info,
+           userId,
         });
         const savedPass = await newPass.save();
 
-        console.log(savedPass);
+       
         
         return NextResponse.json(savedPass);  // Return the saved blog
 
     } catch (error) {
-        console.log('Error:', error);  // Log the actual error message
-        console.log('Error in catch block');
+       
         
         return NextResponse.json({ error: 'An error occurred while saving the blog' }, { status: 500 });  // Return error response
     }
